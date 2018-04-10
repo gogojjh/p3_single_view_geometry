645ecebac1f0fa5fdfb959c0b6ed4f548f3800d2
gogojjh <651650302@qq.com>
2018-04-10 23:17:35 +0800

    delete

--- select_line.m
+++
@@ -1,8 +0,0 @@
-clear; 
-img_ori = imread('image/box.png');
-figure, imshow(img_ori);
-hold on;
-mouse = imline(gca);
-pos = floor(getPosition(mouse));
-disp(pos)
-plot([100,100], [200, 400], 'r-');
